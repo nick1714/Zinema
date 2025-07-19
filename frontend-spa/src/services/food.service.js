f9780a54312956df81d9f1a70f8dc01325150744
Nhut Minh Nguyen <nhutb2205896@student.ctu.edu.vn>
2025-07-19 17:22:13 +0700

    update food images

--- frontend-spa/src/services/food.service.js
+++ frontend-spa/src/services/food.service.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from '@/constants'
+import { API_BASE_URL, STATIC_BASE_URL } from '@/constants'
 
 async function efetch(url, options = {}) {
   let result = {}
@@ -18,6 +18,13 @@ async function efetch(url, options = {}) {
   return json.data
 }
 
+function getFullFoodImageUrl(imagePath) {
+  if (imagePath) {
+    return `${STATIC_BASE_URL}${imagePath}`
+  }
+  return `${STATIC_BASE_URL}/public/images/default-food.png`
+}
+
 class FoodService {
   constructor() {
     this.baseUrl = `${API_BASE_URL}/foods`
@@ -25,11 +32,28 @@ class FoodService {
 
   async getAllFoods(params) {
     const queryParams = new URLSearchParams(params).toString()
-    return efetch(`${this.baseUrl}?${queryParams}`)
+    const data = await efetch(`${this.baseUrl}?${queryParams}`)
+    
+    // Xử lý URL hình ảnh cho tất cả foods
+    if (data.foods && Array.isArray(data.foods)) {
+      data.foods = data.foods.map((food) => ({
+        ...food,
+        image_url: getFullFoodImageUrl(food.image_url),
+      }))
+    }
+    
+    return data
   }
 
   async getFoodById(id) {
-    return efetch(`${this.baseUrl}/${id}`)
+    const data = await efetch(`${this.baseUrl}/${id}`)
+    
+    // Xử lý URL hình ảnh cho food
+    if (data.food) {
+      data.food.image_url = getFullFoodImageUrl(data.food.image_url)
+    }
+    
+    return data
   }
 }
 
